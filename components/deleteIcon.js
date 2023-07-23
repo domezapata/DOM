4890106208603c23069c144fc5668b13f782a777
Dome <domezapata45@gmail.com>
2023-07-23 11:44:24 -0500

    calse 05 completada

---
+++ components/deleteIcon.js
@@ -0,0 +1,12 @@
+const deleteIcon = () => {
+    const i = document.createElement("i");
+    i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+    i.addEventListener("click", deletetask);
+    return i;
+};
+const deletetask = (event) => {
+    console.log("Eliminar tarea");
+    const parent = event.target.parentElement;
+    parent.remove()
+};
+export default deleteIcon;
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( () => { const btn = document.querySelector('[data-form-btn]');
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
     evento.preventDefault();
@@ -17,32 +19,17 @@ const createTask = (evento) => {
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
+    taskContent.appendChild(deleteIcon());
    const content = `
     
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+ `
   //task.innerHTML = content;
   task.appendChild(taskContent);
-
+  task.appendChild(deleteIcon());
   list.appendChild(task);
     
     
 }; 
 
 btn.addEventListener('click', createTask );
-
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
-    return i
-}
-const completeTask = (event) => {
-   
-    const element = event.target
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle('far');
-};
-
-}) ()
 
